19bf566befd3e4cdcadc4c399bfdd5530d4218ae
Keith Lamprecht <1894492+Nixon506E@users.noreply.github.com>
2020-02-26 15:36:27 -0500

    Update denonavr.py
    Support Marantz NR1710

--- denonavr/denonavr.py
+++ denonavr/denonavr.py
@@ -20,7 +20,7 @@ _LOGGER = logging.getLogger("DenonAVR")
 
 DEVICEINFO_AVR_X_PATTERN = re.compile(
     r"(.*AVR-(X|S).*|.*SR500[6-9]|.*SR60(07|08|09|10|11|12|13)|."
-    r"*SR70(07|08|09|10|11|12|13)|.*NR16(04|05|06|07|08|09))")
+    r"*SR70(07|08|09|10|11|12|13)|.*NR16(04|05|06|07|08|09))|.*NR1710")
 DEVICEINFO_COMMAPI_PATTERN = re.compile(r"(0210|0300)")
 
 ReceiverType = namedtuple('ReceiverType', ["type", "port"])
