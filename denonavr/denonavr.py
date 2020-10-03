10937d8eed5c3f94f5e368ed0345d121656c91e7
Mark Suurmond <mark.suurmond@onegini.coml>
2020-10-03 22:24:13 +0200

    Correct names for AURO3D modes

--- denonavr/denonavr.py
+++ denonavr/denonavr.py
@@ -87,6 +87,7 @@ SOUND_MODE_MAPPING = OrderedDict(
                        'DTS + NEURAL:X', 'MULTI CH IN', 'DTS-HD MSTR',
                        'DTS VIRTUAL:X', 'DTS-HD + NEURAL:X', 'DTS-HD',
                        'DTS + VIRTUAL:X']),
+     ('AURO3D', ['AURO-3D', 'AURO-2D SURROUND']),
      ('MCH STEREO', ['MULTI CH STEREO', 'MULTI_CH_STEREO', 'MCH STEREO',
                      'MULTI CH IN 7.1']),
      ('STEREO', ['STEREO']),
