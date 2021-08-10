d159fab3d0538988b69a2c327993033495bf3e26
Lewis Porter <lewis.porter@gmail.com>
2021-08-10 12:27:21 +0200

    Add DTS-HD + DSUR as a sound type

--- denonavr/const.py
+++ denonavr/const.py
@@ -103,7 +103,7 @@ SOUND_MODE_MAPPING = OrderedDict(
                        "DTS + NEURAL:X", "MULTI CH IN", "DTS-HD MSTR",
                        "DTS VIRTUAL:X", "DTS-HD + NEURAL:X", "DTS-HD",
                        "DTS + VIRTUAL:X", "DTS + DOLBY SURROUND",
-                       "DTS-HD + DOLBY SURROUND", "DTS:X MSTR"]),
+                       "DTS-HD + DOLBY SURROUND", "DTS-HD + DSUR", "DTS:X MSTR"]),
      ("AURO3D", ["AURO-3D", "AURO-2D SURROUND"]),
      ("MCH STEREO", ["MULTI CH STEREO", "MULTI_CH_STEREO", "MCH STEREO",
                      "MULTI CH IN 7.1"]),
