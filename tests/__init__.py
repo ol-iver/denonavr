ad32100e1b2b22b7e21f5b911191b8eda14457df
ol-iver <scarface@mywoh.de>
2023-01-15 20:56:40 +0100

    Housekeeping

---
+++ tests/__init__.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Tests of Automation Library for Denon AVR receivers.
+
+:copyright: (c) 2023 by Oliver Goetz.
+:license: MIT, see LICENSE for more details.
+"""
