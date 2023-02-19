db1c2fe8147fb653761f39ea04f9feb3eb3cc206
ol-iver <scarface@mywoh.de>
2023-02-19 22:42:32 +0100

    Create new dev version 0.11.2-dev

--- denonavr/__init__.py
+++ denonavr/__init__.py
@@ -18,7 +18,7 @@ from .decorators import run_async_synchronously
 logging.getLogger(__name__).addHandler(logging.NullHandler())
 
 __title__ = "denonavr"
-__version__ = "0.11.1"
+__version__ = "0.11.2-dev"
 
 
 async def async_discover():
