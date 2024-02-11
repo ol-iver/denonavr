2f18b79ea9cda6ea5c6310300e3767d76e42ec2f
ol-iver <scarface@mywoh.de>
2024-02-11 04:13:45 +0100

    Update to version 0.11.6

--- denonavr/__init__.py
+++ denonavr/__init__.py
@@ -19,7 +19,7 @@ from .ssdp import async_identify_denonavr_receivers
 logging.getLogger(__name__).addHandler(logging.NullHandler())
 
 __title__ = "denonavr"
-__version__ = "0.11.6-dev"
+__version__ = "0.11.6"
 
 
 async def async_discover():
