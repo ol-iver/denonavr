021dec3dd0aa6427a0cd474963df685a1c0b7c7c
Henrik Widlund <4659350+henrikwidlund@users.noreply.github.com>
2026-01-06 12:04:48 +0100

    Combine sound_mode_raw and audio_sound

--- denonavr/denonavr.py
+++ denonavr/denonavr.py
@@ -389,6 +389,8 @@ class DenonAVR(DenonAVRFoundation):
     @property
     def sound_mode_raw(self) -> Optional[str]:
         """Return the current sound mode as string as received from the AVR."""
+        if self.soundmode.sound_mode_raw is None:
+            return self._device.audio_sound
         return self.soundmode.sound_mode_raw
 
     @property
@@ -796,15 +798,6 @@ class DenonAVR(DenonAVRFoundation):
         Only available when use_avr_2016_update is True.
         """
         return self._device.audio_signal
-
-    @property
-    def audio_sound(self) -> Optional[str]:
-        """
-        Return the audio sound for the device.
-
-        Only available when use_avr_2016_update is True.
-        """
-        return self._device.audio_sound
 
     @property
     def audio_sampling_rate(self) -> Optional[str]:
